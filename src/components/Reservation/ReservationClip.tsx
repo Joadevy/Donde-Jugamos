88085fa254c1f4a7eaa8583fffe78a8806022fb3
jjoadevy <jjoaquinarlettaz@gmail.com>
2023-11-03 17:15:19 -0300

    feat: agregar checkeo de sesion iniciada previo a poder iniciar reserva

--- src/components/Reservation/ReservationClip.tsx
+++ src/components/Reservation/ReservationClip.tsx
@@ -1,22 +1,17 @@
+/* eslint-disable @next/next/no-async-client-component */
 /* eslint-disable @typescript-eslint/no-confusing-void-expression */
-"use client";
 import type {Appointment, Court} from "@prisma/client";
 
-import {
-  AlertDialog,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import {getServerSession} from "next-auth";
+
+import {AlertDialog, AlertDialogTrigger} from "@/components/ui/alert-dialog";
 import {timeInStringFromMinutes} from "@/lib/utils/utils";
 import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/hover-card";
+import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
 import {Badge} from "../ui/badge";
 import {Separator} from "../ui/separator";
+import SignInClip from "../Buttons/SignInClip";
 
 import ReservationForm from "./ReservationForm";
 
@@ -36,39 +31,57 @@ interface Iprops {
   sportCenterInfo: SportCenterInformation;
 }
 
-function ReservationClip({appointment, court, sportCenterInfo}: Iprops) {
-  const timeInString = timeInStringFromMinutes(String(appointment.startTime));
+interface IhoverCard {
+  timeInString: string;
+  court: Court;
+}
 
+// Esto deberia estar en un archivo aparte pero como se usa solo aca, quedo.
+function CourtHoverCard({timeInString, court}: IhoverCard) {
   return (
+    <HoverCard>
+      <HoverCardTrigger> {timeInString} hs</HoverCardTrigger>
+      <HoverCardContent>
+        <div className="space-y-1">
+          <div className="flex h-5 justify-between space-x-10 text-sm">
+            <p>Cancha {court.id}</p>
+            <Separator orientation="vertical" />
+            <p>
+              {Number(court.price).toLocaleString("es-AR", {
+                style: "currency",
+                currency: "ARS",
+              })}
+            </p>
+          </div>
+
+          <Separator />
+
+          <p className="font-normal text-slate-700">{court.description}</p>
+        </div>
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
+
+// La idea es que si no inicio sesion, lo redirecciona a iniciar sesion y sino le muestra el que corresponde
+async function ReservationClip({appointment, court, sportCenterInfo}: Iprops) {
+  const timeInString = timeInStringFromMinutes(String(appointment.startTime));
+  const session = await getServerSession(authOptions);
+
+  return session?.user ? (
     <AlertDialog>
       <AlertDialogTrigger>
         <Badge className="hover:cursor-pointer" role="button" variant="default">
-          <HoverCard>
-            <HoverCardTrigger> {timeInString} hs</HoverCardTrigger>
-            <HoverCardContent>
-              <div className="space-y-1">
-                <div className="flex h-5 justify-between space-x-10 text-sm">
-                  <p>Cancha {court.id}</p>
-                  <Separator orientation="vertical" />
-                  <p>
-                    {Number(court.price).toLocaleString("es-AR", {
-                      style: "currency",
-                      currency: "ARS",
-                    })}
-                  </p>
-                </div>
-
-                <Separator />
-
-                <p className="font-normal text-slate-700">{court.description}</p>
-              </div>
-            </HoverCardContent>
-          </HoverCard>
+          <CourtHoverCard court={court} timeInString={timeInString} />
         </Badge>
       </AlertDialogTrigger>
 
       <ReservationForm appointment={appointment} court={court} sportCenterInfo={sportCenterInfo} />
     </AlertDialog>
+  ) : (
+    <SignInClip>
+      <CourtHoverCard court={court} timeInString={timeInString} />
+    </SignInClip>
   );
 }
 
