4c007d89d653d9e6a44066b31a49f1e7fc1e5793
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2023-12-16 20:39:24 -0300

    chore: reinicio de migraciones a la db por problema local

--- prisma/migrations/20231120205952_add_composite_unique_day/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[name,courtId]` on the table `Day` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Day_name_courtId_key" ON "Day"("name", "courtId");
